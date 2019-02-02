a125454c3b0e6517b5c1cec10b86d2205ce07e86
Nik <nik.ignition.work@gmail.com>
2019-02-02 21:45:41 +0200

    deleting unnesssesery derictory

--- gamification/src/main/java/com/gamification/GamificationApplication.java
+++ gamification/src/main/java/com/gamification/GamificationApplication.java
@@ -1,4 +1,4 @@
-package com.gamification.gamification;
+package com.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
